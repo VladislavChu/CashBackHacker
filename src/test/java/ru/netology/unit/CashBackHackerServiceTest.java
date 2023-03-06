c6b2d659ad92114586fdda95b156024d3b903f26
Vladislav Chudaev <vladislav.chudaev1998@gmail.com>
2023-03-06 13:31:57 +0300

    added new branch: testng

--- src/test/java/ru/netology/unit/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/unit/CashBackHackerServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.unit;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.*;
 
 public class CashBackHackerServiceTest {
 
